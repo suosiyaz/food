8dff6c74e79933068f2c34bf8cd36b9835aee532
suosiyaz <siyazputhancode@gmail.com>
2023-05-24 23:04:53 +0530

    Getting Data from Yelp using Axios

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../Components/SearchBar";
 import { useState } from "react";
+import useResults from "../hooks/useResults";
+import ResultsList from "../Components/ResultsList";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [searchApi, results, errorMessage] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        });
+    }
 
     return (
         <View>
-            <SearchBar 
-            term={term} 
-            onTermChange={newTerm => setTerm(newTerm)} 
-            onTermSubmit={() => console.log('term submitted')}
+            <SearchBar
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
             />
-            <Text>{term}</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results</Text>
+            <ResultsList title="Cost Effective" results={filterResultsByPrice('$')} />
+            <ResultsList title="Bit Pricier" results={filterResultsByPrice('$$')} />
+            <ResultsList title="Big Spender" results={filterResultsByPrice('$$$')} />
         </View>
     )
 }
