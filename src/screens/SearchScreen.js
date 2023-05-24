82b3c782f780897a4dfb9f52aea1b56809171ad3
suosiyaz <siyazputhancode@gmail.com>
2023-05-24 17:50:52 +0530

    Assembling a Navigator

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>Search Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
